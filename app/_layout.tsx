aedfd3b4e1ca706eb6081973b9f968cace6af880
FernandoLorca <florcapinto@gmail.com>
2024-05-17 10:06:59 -0400

    Add comment

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack, SplashScreen } from 'expo-router';
 import { useFonts } from 'expo-font';
 import { useEffect } from 'react';
 
+// Prevent the splash screen from hiding until the fonts are loaded
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
