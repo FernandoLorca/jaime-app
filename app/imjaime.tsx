7e375cc2414ae629f14061700a590b22842afb68
FernandoLorca <florcapinto@gmail.com>
2024-05-21 09:21:18 -0400

    Add sounds for Jaime section for yes and no buttons

--- app/imjaime.tsx
+++ app/imjaime.tsx
@@ -1,13 +1,42 @@
+import { useState } from 'react';
 import { Text, View } from 'react-native';
 import ToastManager, { Toast } from 'toastify-react-native';
+import { Audio } from 'expo-av';
 import { useJaimeNotificationState } from '@/store/JaimeNotificationState';
 import ButtonJaime from '@/components/ButtonJaime';
 
 export default function ImJaime() {
   const setJaimeState = useJaimeNotificationState(state => state.setJaimeState);
+  const [sound, setSound] = useState<Audio.Sound | null>(null);
 
   const showNotification = () => {
     Toast.success('Notificación enviada');
+  };
+
+  const playYesSound = async () => {
+    try {
+      const { sound } = await Audio.Sound.createAsync(
+        require('../assets/audio/yes.mp3')
+      );
+      setSound(sound);
+
+      await sound.playAsync();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const playNoSound = async () => {
+    try {
+      const { sound } = await Audio.Sound.createAsync(
+        require('../assets/audio/no.mp3')
+      );
+      setSound(sound);
+
+      await sound.playAsync();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -54,8 +83,18 @@ export default function ImJaime() {
         />
         <Text className="pb-2 text-lg">Respuesta:</Text>
         <View className="flex flex-row justify-between">
-          <ButtonJaime label="✔️ Sí" />
-          <ButtonJaime label="❌ No" />
+          <ButtonJaime
+            label="✔️ Sí"
+            isPressed={() => {
+              playYesSound();
+            }}
+          />
+          <ButtonJaime
+            label="❌ No"
+            isPressed={() => {
+              playNoSound();
+            }}
+          />
         </View>
       </View>
     </View>
